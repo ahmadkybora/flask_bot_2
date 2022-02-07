d34e7edcc8add4729319ee9097d52fb55b14d821
montazeri_a <montazeri_a@office.zharfpouyan.net>
2022-02-07 11:22:45 +0330

    =nwe

--- test.py
+++ test.py
@@ -1,13 +1,34 @@
-from flask import Flask
 import os
+import requests
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "ok"
+token = '2016260844:AAGwWwI6ZLA7cLUNNcAbbFz2W84wkJebZyo'
+url = "https://api.telegram.org/bot" + token + "/"
 
-home()
+@app.route("/")
+def get_updates_json(request):  
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+def last_update(data):  
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+def get_chat_id(update):  
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):  
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+chat_id = get_chat_id(last_update(get_updates_json(url)))
+
+send_mess(chat_id, 'Your message goes here')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
